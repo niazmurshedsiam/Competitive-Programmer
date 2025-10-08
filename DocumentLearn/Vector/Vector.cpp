e1a6013cb6998d1375365287e8b78646783af67e
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2025-10-09 00:23:59 +0600

    vector method

--- DocumentLearn/Vector/Vector.cpp
+++ DocumentLearn/Vector/Vector.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    vector<int> c = {1,2,3,4};
+   // vector<int> c = {1,2,3,4};
     //cout<<c.size()<<endl;
     //c.push_back(6);
     //cout<<c.back();
@@ -11,11 +11,24 @@ int main()
     //cout<<c.front();
     //c.clear();
     //c.size();
-    if(c.empty()==true){
-        cout<<"Yes";
-    }
-    else{
-        cout<<"No";
-    }
+    //if(c.empty()==true){
+      //  cout<<"Yes";
+    //}
+    //else{
+      //  cout<<"No";
+    //}
+    vector<int> c = {3,5,2,4,10,9};
+    //sort(c.begin(),c.end());
+   // sort(c.begin(),c.end(),greater<int>());
+   //sort(c.begin()+1,c.end());
+   //reverse(c.begin(),c.end());
+    //for(int i=0;i<c.size();i++){
+      //  cout<<c[i]<< " ";
+    //}
+
+    auto max = *max_element(c.begin(),c.end());
+    cout<<max <<endl;
+    auto min = *min_element(c.begin(),c.end());
+    cout<<min <<endl;
     return 0;
 }
