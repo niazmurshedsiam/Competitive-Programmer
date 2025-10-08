92980f05b39d339899392c33db469b2d5aff0e5f
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2025-10-09 00:49:34 +0600

    Find maximum in an Array codechef

---
+++ DocumentLearn/Vector/FindMaximumInAnArray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        int max = *max_element(v.begin(),v.end());
+        cout<<max<<endl;
+    }
+
+
+}
