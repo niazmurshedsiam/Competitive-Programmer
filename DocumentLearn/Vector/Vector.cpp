1a01a9d23fd314598e958c9892319027e5834b8b
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2025-10-08 23:51:08 +0600

    vector

---
+++ DocumentLearn/Vector/Vector.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> c = {1,2,3,4};
+    //cout<<c.size()<<endl;
+    //c.push_back(6);
+    //cout<<c.back();
+    //c.pop_back();
+    //cout<<c.size();
+    //cout<<c.front();
+    //c.clear();
+    //c.size();
+    if(c.empty()==true){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
