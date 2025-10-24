8ef00f61dadb5621f952b4f4aaafaf187975a231
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2025-10-24 23:35:30 +0600

    contest 2 last 12 problem solve

---
+++ AUTN_Sheet01/E_AreaofaCircle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    double area,r,pi=3.141592653;
+    cin>>r;
+    area = pi*r*r;
+    cout<<fixed << setprecision(9) << area <<endl;
+
+    return 0;
+}
---
+++ AUTN_Sheet01/F_DigitsSummation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    long long n,m;
+    cin>>n>>m;
+    long long r = n%10;
+    long long s = m%10;
+
+    cout<< r+s <<endl;
+
+    return 0;
+}
