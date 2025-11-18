f13c060b9144ea2ace197263abdc199057de16dc
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2025-11-18 23:47:19 +0600

    array

--- Contest_4_Array/L_M.c
+++ Contest_4_Array/L_M.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
 
+int main() {
+
+    int t;
+    scanf("%d", & t);
+
+    while (t--) {
+        int n;
+        scanf("%d", & n);
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", & arr[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i]++;
+            int m = 1;
+            for (int j = 0; j < n; j++) {
+                m *= arr[j];
+            }
+            if (m > ans) {
+                ans = m;
+            }
+            arr[i]--;
+        }
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
--- Contest_4_Array/M_N.c
+++ Contest_4_Array/M_N.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
 
+int main() {
+
+    int t;
+    scanf("%d", & t);
+
+    while (t--) {
+        int n;
+        scanf("%d", & n);
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", & arr[i]);
+        }
+
+        int F[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            F[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            F[arr[i]]++;
+        }
+
+        int ans = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (F[i] >= 3) {
+                ans = i;
+                break;
+            }
+        }
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
