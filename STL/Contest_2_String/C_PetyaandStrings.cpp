fc8f7a53ee69e593561e92a66610f39ea4c46d5e
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2026-01-28 23:02:13 +0600

    string

---
+++ STL/Contest_2_String/C_PetyaandStrings.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    int n= a.size();
+    for(int i=0;i<n;i++){
+        a[i]=tolower(a[i]);
+    }
+    for(int i=0;i<n;i++){
+        b[i]=tolower(b[i]);
+    }
+    if(a==b){
+        cout<<"0";
+    }
+    else if(a>b){
+        cout<<"1";
+    }
+    else{
+        cout<<"-1";
+    }
+    return 0;
+}
